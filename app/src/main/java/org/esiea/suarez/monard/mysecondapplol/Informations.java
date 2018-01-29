2109110c549e0f73cc87e7fda742c9225d9141fb
LoicMonard <bobbycarotte@outlook.fr>
2018-01-29 12:21:18 +0100

    Action bar button + notification

---
+++ app/src/main/java/org/esiea/suarez/monard/mysecondapplol/Informations.java
@@ -0,0 +1,18 @@
+package org.esiea.suarez.monard.mysecondapplol;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bobby on 29/01/2018.
+ */
+
+public class Informations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_informations);
+    }
+}
