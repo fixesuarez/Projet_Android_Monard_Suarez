8b44431c0c7bd7b2fda98fbfff0bd2f3f611d853
LoicMonard <bobbycarotte@outlook.fr>
2018-01-29 14:45:36 +0100

    Call externe app of the phone

--- app/src/main/java/org/esiea/suarez/monard/mysecondapplol/Informations.java
+++ app/src/main/java/org/esiea/suarez/monard/mysecondapplol/Informations.java
@@ -1,7 +1,11 @@
 package org.esiea.suarez.monard.mysecondapplol;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Bobby on 29/01/2018.
@@ -14,5 +18,16 @@ public class Informations extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_informations);
+
+        Button btn = (Button)findViewById(R.id.open_phone);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0123456789"));
+                startActivity(intent);
+            }
+        });
     }
 }
